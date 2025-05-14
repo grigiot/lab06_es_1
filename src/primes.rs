46c9d87b1d75438003e305c51cfa430701e4914b
grigiot <s336224@studenti.polito.it>
2025-05-14 17:32:48 +0200

    feat(test): test is prime

--- src/primes.rs
+++ src/primes.rs
@@ -12,3 +12,16 @@ pub fn is_prime(n: u64) -> bool {
 
 pub fn find_primes(limit: u64, n_threads: u64) -> Vec<u64> {
 }
+
+#[cfg(test)]
+mod test{
+    use crate::primes::is_prime;
+
+
+    #[test]
+    fn test_is_prime(){
+        assert!(is_prime(2));
+        assert!(!is_prime(78))
+    }
+
+}
