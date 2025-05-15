7e782752444566403bbf81d981d85e3b6e0debdd
grigiot <s336224@studenti.polito.it>
2025-05-15 18:33:03 +0200

    feat(counter): version with counter implemented

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use primes::find_primes;
+
 mod primes;
 fn main() {
-    println!("Hello, world!");
+    let v = find_primes(100,  4);
+    println!("PRIME NUMBERS LIMIT 67 THREADS 5: {:?}", v);
 }
--- src/primes.rs
+++ src/primes.rs
@@ -1,3 +1,7 @@
+use core::time;
+use std::sync::{Arc, Mutex};
+use std::thread::{sleep, JoinHandle};
+
 pub fn is_prime(n: u64) -> bool {
     if n < 2 {
         return false;
@@ -11,6 +15,47 @@ pub fn is_prime(n: u64) -> bool {
 }
 
 pub fn find_primes(limit: u64, n_threads: u64) -> Vec<u64> {
+    
+    let mut jh_vec: Vec<JoinHandle<Vec<u64>>> = vec![];
+
+    let counter: Arc<Mutex<u64>> = Arc::new(Mutex::new(2));
+    for _i in 0..=n_threads {
+        let cnt: Arc<Mutex<u64>> = counter.clone();
+
+        jh_vec.push(std::thread::spawn(move ||{
+            let mut res_vec: Vec<u64> = vec![];
+            // let mut first_loop = true;
+            loop{
+                let mut p: std::sync::MutexGuard<'_, u64> = cnt.lock().unwrap();
+                
+                let n: u64 = *p;
+                *p += 1;
+                drop(p);
+                // if first_loop{
+                //     sleep(time::Duration::from_secs(7));
+                //     first_loop = false;
+                // }
+
+                if n>limit{
+                    break;
+                }
+                if is_prime(n) {
+                    res_vec.push(n);
+                }    
+            }
+            println!("THREAD {:?} -> res_vec: {:?}", std::thread::current().id(), res_vec);
+            res_vec
+        }));
+    }
+    
+    let result = jh_vec.into_iter().map(|jh| {
+        jh.join().unwrap()
+    }).fold(Vec::new(), |mut acc: Vec<u64>, mut e|{
+        acc.append(&mut e);
+        return acc;
+    });
+
+    result
 }
 
 #[cfg(test)]
